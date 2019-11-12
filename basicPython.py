2217c54da39b2b2154a60efd9f20d43944bcfa04
spikpe <parithi37@gmail.com>
2019-11-12 22:23:03 +0530

    12NovCommit

---
+++ basicPython.py
@@ -0,0 +1,31 @@
+# print('Hello world')
+
+# #List
+# coursesList = ['Math','Science','Comp']
+
+# for subjects in coursesList:
+#     print(subjects)
+
+# for index, subjects in enumerate(coursesList, start = 1):
+#     print(index, subjects)
+
+
+# #Tuples
+# coursesTuple = ('Math','Science','Comp')
+
+# for subjects in coursesTuple:
+#     print(subjects)
+# print(type(coursesTuple))
+
+# #Sets
+# coursesSets = {'Math','Science','Comp'}
+
+# for subjects in coursesSets:
+#     print(subjects)
+# print(type(coursesSets))
+
+
+#Dictionary
+student = {'name':'Raj','course':'EandI','College':'KCT'}
+
+print(student['College'])
---
+++ if_and_loop.py
@@ -0,0 +1,21 @@
+# if True:
+#     print("the above is true")
+
+# language = 'python'
+
+# if(language=='python'):
+#     print("The language is "+language)
+
+language = 'ruby'
+
+if(language=='python'):
+    print("The language is "+language)
+elif language=='ruby':
+    print("the language is "+language)
+else:
+    print("the language is not python")
+
+
+
+
+
