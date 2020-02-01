1495e007822c5c182f7ed1aca39f2fbe1effe1b5
spikpe <parithi37@gmail.com>
2020-02-01 13:59:23 +0530

    pyTestFrameworkPractice

---
+++ math_func.py
@@ -0,0 +1,6 @@
+def addition(x,y):
+    return x+y
+
+def product(x,y):
+    return x*y
+#print(addition(2,3), product(2,3))
---
+++ test_math_func.py
@@ -0,0 +1,10 @@
+import math_func
+
+def test_add():
+    assert math_func.addition(7,4) == 11
+    assert math_func.addition(4,6) == 10
+
+def test_product():
+    assert math_func.product(2,5) == 10
+    assert math_func.product(8,9) == 70
+
